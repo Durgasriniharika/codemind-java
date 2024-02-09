545e72f5a5c4fb1cb1c81f9048d2af97a5340ee3
Durgasriniharika <142823558+Durgasriniharika@users.noreply.github.com>
2024-02-09 12:12:58 +0530

    import java.util.*; public class Example {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         char[] arr=s.toCharArray();         System.out.println(arr[6]);     } }

---
+++ Lucky_Character.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Example
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        char[] arr=s.toCharArray();
+        System.out.println(arr[6]);
+    }
+}
